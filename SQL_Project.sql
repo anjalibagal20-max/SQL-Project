51ee5887ad26e3b75165ce4331e1288558f71773
Anjali Bagal <anjalibagal20@gmail.com>
2025-10-05 09:18:41 +0530

    Add files via upload

--- SQL_Project.sql
+++ SQL_Project.sql
@@ -51,6 +51,18 @@ select * from books order by stock asc limit 5;
 
 select sum(revenue) as Total_revenue from Orders;
 
+select auother, count(Book_id) as totalbooks from books
+group by auother
+order by totalbooks desc;
+ 
+ select sum(Total_Amount) as TotalRevenue from Orders;
+ 
+select genre,avg(price) as AveragePrice from Books
+group by genre
+order by Averageprice desc; 
+
+select max(price) as HighestPrice, min(price) as LowestPrice from Books;
+ 
 select b.genre,sum(o.quantity) from Orders o
 join books b on b.book_id=o.book_id 
 group by b.genre;
@@ -80,7 +92,73 @@ join customers c on c.customer_id=o.order_id
  join customers c on o.customer_id=c.customer_id
  where o.Total_amount >30;
  
+select author, count(bookID) as totalbooks from books
+group by author
+order by totalbooks desc;
+
+select B.Genre,sum(O.Quantity*B.price) as TotalGenreRevenue from Books B
+inner join Orders O ON b.Book_id=O.Book_id
+group by B.Genre;
+
+select B.title, B.auother,
+SUM(O.Quantity) AS TotalQuantitySold
+from Books B
+inner join
+    Orders O ON B.Book_id = O.Book_id
+group by
+    B.Book_id, B.title, B.auother
+order by TotalQuantitySold DESC
+LIMIT 5;
+
+SELECT C.Name,
+COUNT(O.Order_ID) AS TotalOrders,
+AVG(O.Total_Amount) AS AverageOrderValue
+FROM Customers C
+INNER JOIN
+    Orders O ON C.Customer_ID = O.Customer_ID
+GROUP BY
+    C.Customer_ID, C.Name
+HAVING
+    AVG(O.Total_Amount) > 1000 
+ORDER BY
+    AverageOrderValue DESC;
+
+
+SELECT B.title, B.Genre, SUM(O.Quantity) AS TotalQuantitySold,
+RANK() OVER (PARTITION BY B.Genre
+ORDER BY SUM(O.Quantity) DESC) AS SalesRankInGenre
+FROM Books B
+INNER JOIN Orders O ON B.Book_id = O.Book_id
+GROUP BY B.Book_id, B.title, B.Genre 
+ORDER BY B.Genre, SalesRankInGenre;
  
+
+ SELECT Order_Date AS SalesMonth,
+SUM(Total_Amount) AS MonthlyTotal
+FROM Orders
+GROUP BY SalesMonth;
+
+ SELECT Title,Genre,Price,
+ AVG(Price) OVER (PARTITION BY Genre) AS GenreAveragePrice,
+ Price - AVG(Price) OVER (PARTITION BY Genre) AS PriceDifferenceFromAverage
+FROM Books
+ORDER BY Genre, Price DESC;
+
+ SELECT Genre, price,
+SUM(price) OVER () AS OverallTotalPrice,
+(price * 100.0) / SUM(price) OVER () AS PercentageOfTotalPrice
+FROM Books
+ORDER BY PercentageOfTotalPrice DESC limit 5;
  
+ SELECT C.Customer_ID,C.Name,
+SUM(O.Total_Amount) AS TotalLifetimeSpend,
+DENSE_RANK() OVER (ORDER BY SUM(O.Total_Amount) DESC) AS CustomerValueRank
+FROM Customers C
+INNER JOIN
+    Orders O ON C.Customer_ID = O.Customer_ID
+GROUP BY
+    C.Customer_ID, C.Name
+ORDER BY
+    CustomerValueRank, TotalLifetimeSpend DESC;
  
  
